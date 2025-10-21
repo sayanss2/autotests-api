e2e08f143cfd2526ade5efbb6e301fc3761ac37d
sayanss2 <kolosovsky.al@yandex.ru>
2025-10-21 14:15:19 +0700

    Введение в HTTPX. Используем HTTPX для работы с /api/v1/authentication API

--- httpx_authentication.py
+++ httpx_authentication.py
@@ -9,9 +9,6 @@ login_payload = {
 login_response = httpx.post("http://127.0.0.1:8000/api/v1/authentication/login", json=login_payload)
 login_response_data = login_response.json()
 
-access_token = login_response_data.get("token", {}).get("accessToken")
-refresh_token = login_response_data.get("token", {}).get("refreshToken")
-
 print("URL:", login_response.url,  "\n")
 print("Login response:", json.dumps(login_response_data, indent=2), "\n")
 print("Status code:", login_response.status_code, "\n")
@@ -19,6 +16,7 @@ print("Status code:", login_response.status_code, "\n")
 refresh_payload = {
   "refreshToken": login_response_data["token"]["refreshToken"]
 }
+
 refresh_response = httpx.post("http://127.0.0.1:8000/api/v1/authentication/refresh", json=refresh_payload)
 refresh_response_data = refresh_response.json()
 
