20bd950f4d0f164b48a81f3f3015d58ced4c4a7a
sayanss2 <kolosovsky.al@yandex.ru>
2025-10-21 16:47:56 +0700

    Практикуемся в работе с HTTPX. Практикуемся в работе с HTTPX: удаление пользователя

--- tools/log_utils.py
+++ tools/log_utils.py
@@ -5,8 +5,14 @@ def log_response(response, label="Response") -> None:
     """Выводит на печать ответ с метками"""
     print(f"URL: {response.url}")
     print(f"Status code: {response.status_code}")
+    print(f"Method: {response.request.method}")
     try:
-        print(f"{label}:", json.dumps(response.json(), indent=2))
+        data = response.json()
+    # Если тело ответа пустое или равно null, выводим более понятное сообщение
+        if data is None or data == {} or data == []:
+            print(f"{label}: <нет данных>")
+        else:
+            print(f"{label}:", json.dumps(data, indent=2))
     except Exception:
         print(f"{label}: <не JSON>")
     print("="*80)
