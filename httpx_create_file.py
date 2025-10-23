7d386cbeb2ffb8abad31b1eda3e1d44b55ac26cb
sayanss2 <kolosovsky.al@yandex.ru>
2025-10-23 15:27:46 +0700

    Практикуемся в реализации API клиентов. Практика реализации API клиентов: /api/v1/files. Часть 1 — Загрузка файла

---
+++ httpx_create_file.py
@@ -0,0 +1,48 @@
+import httpx
+
+from tools import log_response
+from tools import get_random_email
+
+SERVER = "http://127.0.0.1:8000"
+
+client = httpx.Client(base_url=SERVER)
+
+create_user_payload = {
+  "email": get_random_email(),
+  "password": "string",
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string"
+}
+
+user_create_response = client.post("/api/v1/users", json = create_user_payload)
+user_create_data = user_create_response.json()
+log_response(user_create_response, "User create")
+#user_id = user_create_data["user"]["id"]
+
+
+login_payload = {
+    "email": create_user_payload["email"],
+    "password": create_user_payload["password"]
+}
+
+user_login_response = client.post("/api/v1/authentication/login", json = login_payload)
+user_login_data = user_login_response.json()
+#log_response(user_login_response, "User login (tokens)")
+
+headers_credentials = {
+        "Authorization": f'Bearer ' + user_login_data["token"]["accessToken"]
+    }
+
+
+create_file_response = client.post(
+    "/api/v1/files",
+    data={"filename": "image.png", "directory": "images"},
+    files={"upload_file": open('./testdata/files/testfile.png', 'rb')},
+    headers = headers_credentials
+)
+create_file_data = create_file_response.json()
+log_response(create_file_response, "Create file")
+
+
+client.close()
