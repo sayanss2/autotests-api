9eaf5aeeabf18de43d592462b5f8a8d9ad61f6ab
sayanss2 <kolosovsky.al@yandex.ru>
2025-11-02 14:00:01 +0700

    Маркировки Pytest. Запускаем API автотесты по маркировкам

--- tests/test_pytest_markers.py
+++ tests/test_pytest_markers.py
@@ -1,5 +1,6 @@
 import pytest
 
+"""
 @pytest.mark.authentication
 class TestUserAuthentication:
     
@@ -37,3 +38,39 @@ class TestUserInterface:
     @pytest.mark.smoke
     def test_signup(self):
         pass
+"""
+
+
+@pytest.mark.smoke
+class TestLogin:
+    @pytest.mark.smoke
+    def test_valid_login(self):
+        pass
+
+    @pytest.mark.regression
+    def test_invalid_login(self):
+        pass
+
+@pytest.mark.regression
+class TestRegistration:
+    @pytest.mark.regression
+    def test_valid_registration(self):
+        pass
+
+    @pytest.mark.smoke
+    def test_invalid_registration(self):
+        pass
+
+@pytest.mark.smoke
+@pytest.mark.regression
+class TestCheckout:
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_valid_checkout(self):
+        pass
+
+    def test_invalid_checkout(self):
+        pass
+
+def test_search():
+    pass
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,8 @@
 #tests/test_users.py
 from http import HTTPStatus
 
+import pytest  # Импортируем библиотеку pytest
+
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import (
     CreateUserRequestSchema,
@@ -11,6 +13,8 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_create_user_response
 
 
+@pytest.mark.users  # Добавили маркировку users
+@pytest.mark.regression  # Добавили маркировку regression
 def test_create_user():
     # Инициализируем API-клиент для работы с пользователями
     public_users_client = get_public_users_client()
