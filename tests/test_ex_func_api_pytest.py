eb5f08ac2fd6e01840a8c925950ef3409eb34996
sayanss2 <kolosovsky.al@yandex.ru>
2025-11-15 12:03:54 +0700

    Фикстуры Pytest. Фикстуры в pytest

--- tests/test_ex_func_api_pytest.py
+++ tests/test_ex_func_api_pytest.py
@@ -1,29 +1,34 @@
 # tests/test_example_api_pytest.py
 import pytest
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.private_users_client import get_private_users_client
-from clients.users.public_users_client import get_public_users_client
+from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
+from clients.users.public_users_client import PublicUsersClient, get_public_users_client
 from clients.users.users_schema import (
-    CreateUserRequestSchema, 
+    CreateUserRequestSchema,
+    CreateUserResponseSchema, 
     GetUserResponseSchema
 )
 
 
 @pytest.fixture
-def public_client():
+def public_client() -> PublicUsersClient:
     """Возвращает публичного клиента для создания пользователей."""
     return get_public_users_client()
 
 
 @pytest.fixture
-def create_user_request():
+def create_user_request() -> CreateUserRequestSchema:
     """Генерируем данные запроса на создание пользователя."""
     return CreateUserRequestSchema()
 
 
 @pytest.fixture
-def created_user(public_client, create_user_request):
+def created_user(
+    public_client: PublicUsersClient, 
+    create_user_request: CreateUserRequestSchema
+) -> CreateUserResponseSchema:
     """Создаём пользователя через PublicUsersClient и возвращаем ответ."""
+
     response = public_client.create_user(create_user_request)
     # Проверяем, что пользователь успешно создан
     assert response is not None
@@ -32,23 +37,24 @@ def created_user(public_client, create_user_request):
 
 
 @pytest.fixture
-def private_client(create_user_request):
+def private_client(create_user_request: CreateUserRequestSchema) -> PrivateUsersClient:
     """Возвращаем приватного клиента для авторизованного пользователя."""
     auth_user = AuthenticationUserSchema(
         email=create_user_request.email,
         password=create_user_request.password
-    )
+)
     return get_private_users_client(auth_user)
 
 
 # Тесты
-@pytest.mark.skip(reason="Для тестового запуска")
-def test_create_and_get_user(created_user, private_client):
+#@pytest.mark.skip(reason="Для тестового запуска")
+def test_create_and_get_user(created_user: CreateUserResponseSchema, private_client: PrivateUsersClient):
     """Сценарий: создаём пользователя и получаем его через приватный API."""
     user_id = created_user.user.id
 
     # Используем метод get_user
-    get_user_response: GetUserResponseSchema = private_client.get_user(user_id)
+    #get_user_response: GetUserResponseSchema = private_client.get_user(user_id)
+    get_user_response = private_client.get_user(user_id)
 
     # Проверки
     assert get_user_response is not None
