1475c5d44f3d8912078d59194069932026521e77
sayanss2 <kolosovsky.al@yandex.ru>
2025-11-18 17:05:35 +0700

    Параметризация Pytest. Практикуемся с параметризацией pytest

---
+++ tests/test_parametrization.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.parametrize("number", [1, 2, 3, -1])
+def test_numbers(number: int):
+    assert number > 0
+
+
+@pytest.mark.parametrize("number, expected", [(1, 1), (2, 4), (3, 9)])
+def test_several_numbers(number: int, expected: int):
+    assert number ** 2 == expected
+
+
+@pytest.mark.parametrize("os", ["macos", "windows", "linux", "debian"])
+@pytest.mark.parametrize("host", [
+    "https://dev.company.com",
+    "https://stable.company.com",
+    "https://prod.company.com"
+])
+def test_multiplication_of_numbers(os: str, host: str):
+    assert len(os + host) > 0
